464e880212419118c2096806d7f0335c7bd89d13
harrym8n <harrym8n@naver.com>
2024-10-28 15:52:02 +0900

    Delete 프로그래머스(Programmers)/Lv2 directory

--- 프로그래머스(Programmers)/Lv2/동명 동물 수 찾기_Finding the number of animals of the same name.sql
+++
@@ -1,7 +0,0 @@
-#programmers Lv2 문제 - 동명 동물 수 찾기(Finding the number of animals of the same name)
-
-SELECT NAME, COUNT(NAME) as COUNT
-FROM ANIMAL_INS
-GROUP BY name
-HAVING COUNT >= 2
-ORDER BY NAME
--- 프로그래머스(Programmers)/Lv2/이름에 'el'이 들어가는 동물 찾기.sql
+++
@@ -1,5 +0,0 @@
-#Find an animal with an 'el' in its name
-SELECT ANIMAL_ID, NAME
-FROM ANIMAL_INS as AI
-WHERE NAME LIKE '%el%' and ANIMAL_TYPE = 'Dog'
-ORDER BY NAME
