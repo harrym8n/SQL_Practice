9e5a90a35a9d76fb2a5e9c3d61455f16319e4a44
harrym8n <harrym8n@naver.com>
2024-11-24 23:57:11 +0900

    Solve quizs on 241124

---
+++ Programmers_Quiz/Lv3/241124_자동차_대여기록에서_대여중_대여가능_여부_구분하기.sql
@@ -0,0 +1,20 @@
+# 2022년 10월 16일에 대여 중인 자동차인 경우 '대여중' 이라고 표시
+# 대여 중이지 않은 자동차인 경우 '대여 가능'을 표시하는 컬럼(컬럼명: AVAILABILITY)을 추가하여 자동차 ID와 AVAILABILITY 리스트를 출력
+# 반납 날짜가 2022년 10월 16일인 경우에도 '대여중'으로 표시
+# 자동차 ID를 기준으로 내림차순 정렬
+
+SELECT 
+    CARS.CAR_ID,
+    CASE 
+        WHEN RENTED.CAR_ID IS NOT NULL THEN '대여중'
+        ELSE '대여 가능'
+    END AS AVAILABILITY
+FROM 
+    (SELECT DISTINCT CAR_ID FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY) AS CARS
+LEFT JOIN 
+    (SELECT CAR_ID 
+     FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+     WHERE START_DATE <= '2022-10-16' AND '2022-10-16' <= END_DATE
+    ) AS RENTED 
+ON CARS.CAR_ID = RENTED.CAR_ID
+ORDER BY CARS.CAR_ID DESC;
