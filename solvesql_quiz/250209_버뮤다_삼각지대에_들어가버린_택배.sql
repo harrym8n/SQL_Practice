335e67e191acc542d92fd978ee4db1f36a2ca9f9
harrym8n <harrym8n@naver.com>
2025-02-09 17:10:46 +0900

    Solve quiz1 on 250209

---
+++ solvesql_quiz/250209_버뮤다_삼각지대에_들어가버린_택배.sql
@@ -0,0 +1,11 @@
+SELECT
+  DATE(order_delivered_carrier_date) AS delivered_carrier_date, COUNT(*) AS orders
+FROM
+  olist_orders_dataset
+WHERE
+  order_delivered_carrier_date LIKE '2017-01%' AND
+  order_delivered_customer_date IS NULL
+GROUP BY
+  DATE(order_delivered_carrier_date)
+ORDER BY
+  delivered_carrier_date
